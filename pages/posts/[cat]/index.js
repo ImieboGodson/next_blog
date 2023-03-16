adfcdaa3ebca4069f697887f05d4350e5d4e514f
Godson Imiebo <imiebo.godson@gmail.com>
2023-03-16 23:04:51 +0100

    added data and static assets

--- pages/posts/[cat]/index.js
+++ pages/posts/[cat]/index.js
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import React from "react";
 
 export default function Category() {
-  return <div>Category</div>;
+  return (
+    <div>
+      <Head>
+        <title>Post Categories</title>
+      </Head>
+      <main>HELOO</main>
+    </div>
+  );
 }
