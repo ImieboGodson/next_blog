4c53c32b4cab5e3e2b3eec153fa4441521537fd8
Godson Imiebo <imiebo.godson@gmail.com>
2023-03-21 14:27:35 +0100

    refactor: added title to category page and main page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import PostsLayout from "@/components/PostsLayout";
 import PostCardsLayout from "@/components/PostCardsLayout";
-import { PostCard } from "@/components/PostCard";
-import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ posts }) {
-  // useEffect(() => {
-  //   console.log(posts);
-  // }, []);
-
   return (
     <>
       <Head>
@@ -34,8 +27,7 @@ export default function Home({ posts }) {
 
 export async function getStaticProps(context) {
   const response = await fetch("http://localhost:3000/api/posts");
-  const { message, posts } = await response.json();
-  // console.log(posts);
+  const { posts } = await response.json();
 
   return {
     props: {
--- pages/posts/category/[cat]/index.js
+++ pages/posts/category/[cat]/index.js
@@ -1,13 +1,17 @@
 import PostCardsLayout from "@/components/PostCardsLayout";
 import PostsLayout from "@/components/PostsLayout";
+import formatCategory from "@/lib/formatCategory";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function Category({ posts }) {
+  const { query } = useRouter();
+  const categoryText = formatCategory(query.cat);
   return (
     <div>
       <Head>
-        <title>Post Categories</title>
+        <title>{`Category: ${categoryText} | MetalBox`}</title>
       </Head>
       <main className="w-full min-h-fit flex items-top justify-center text-stone-700">
         <PostsLayout>
