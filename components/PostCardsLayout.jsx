0b268155db0030ea352a02a0ecc5f61c39e99f71
Godson Imiebo <imiebo.godson@gmail.com>
2023-03-21 13:23:35 +0100

    fix: refactored the code for the  PostCardsLayout component

--- components/PostCardsLayout.jsx
+++ components/PostCardsLayout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { PostCard } from "./PostCard";
 
-export default function PostCardsLayout({ children }) {
+export default function PostCardsLayout({ posts }) {
   return (
     <div className="w-full min-h-fit py-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-20 grid-layout">
-      {children}
+      {posts.map((post) => {
+        return <PostCard key={posts[post]} post={post} />;
+      })}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,7 @@ export default function Home({ posts }) {
       <main className="w-full min-h-fit flex items-top justify-center text-stone-700">
         <PostsLayout>
           <main className=" mt-8">
-            <PostCardsLayout>
-              {posts.map((post) => {
-                return <PostCard key={posts[post]} post={post} />;
-              })}
-            </PostCardsLayout>
+            <PostCardsLayout posts={posts} />
           </main>
         </PostsLayout>
       </main>
