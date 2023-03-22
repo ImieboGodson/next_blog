b09c08e769df2c3bf871f367d5d4f69b9d2b1c9f
Godson Imiebo <imiebo.godson@gmail.com>
2023-03-22 13:18:47 +0100

    feat: made post page statically generated

--- pages/posts/category/[cat]/[slug].js
+++ pages/posts/category/[cat]/[slug].js
@@ -1,5 +1,36 @@
 import React from "react";
 
-export default function Post() {
-  return <div>Post</div>;
+export default function Post({ post }) {
+  return <div>{post}</div>;
+}
+
+export async function getStaticProps(context) {
+  const { cat, slug } = context.params;
+  const response = await fetch(
+    `http://localhost:3000/api/posts/category/${cat}/${slug}`
+  );
+  const { post } = await response.json();
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const response = await fetch("http://localhost:3000/api/posts");
+  const { posts } = await response.json();
+  const paths = posts.map((post) => {
+    return {
+      params: {
+        cat: post.data.tag,
+        slug: post.realSlug,
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
 }
