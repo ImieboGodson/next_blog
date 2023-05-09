e3c6a6895a3185bbd7e3e4892c6c23ab6a7ee898
Godson Imiebo <imiebo.godson@gmail.com>
2023-05-09 08:51:42 +0100

    fix: minor fixes on post page

--- pages/category/[cat]/[slug].js
+++ pages/category/[cat]/[slug].js
@@ -30,13 +30,13 @@ export default function Post({ post, relatedPosts }) {
           </Head>
           <main className="">
             <div className="relative flex justify-center items-start p-10 bg-[#00235B] text-white">
-              <span className="absolute left-[4%] lg:left-[8%] bottom-[25%] lg:bottom-[10%] rotate-12 flex justify-center items-center text-[#FF6D60]">
+              <span className="absolute left-[4%] lg:left-[8%] bottom-[23%] lg:bottom-[10%] rotate-12 flex justify-center items-center text-[#FF6D60]">
                 <FiStar className="text-[25px] lg:text-[38px]" />
               </span>
               <span className="absolute right-[10%] top-[10%] lg:top-[20%] rotate-45 flex justify-center items-center text-[#00FFCA]">
                 <FiTriangle className="text-[25px] lg:text-[38px]" />
               </span>
-              <div className="w-[90%] lg:w-[70%] h-fit md:h-[250px] pt-4 pb-20 md:py-6 flex flex-col md:flex-row justify-between items-start">
+              <div className="w-[99%] lg:w-[70%] h-fit md:h-[250px] pt-[0.8rem] pb-[4rem] md:py-6 flex flex-col md:flex-row justify-between items-start">
                 <div className="">
                   <Link href={`/category/${data.tag}/`}>
                     <p className="text-sm font-normal text-[#19A7CE]">
